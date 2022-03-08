45028b4d122dd5af0f08df8d067c2627f2279c89
Luiz Satto <luizf.satto@gmail.com>
2022-03-08 18:16:27 -0300

    Added Components files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { CssBaseline, Grid } from '@material-ui/core'
+
+import Header from './components/Header';
+import List from './components/List';
+import Map from './components/Map';
 
 const App: React.FC = () => {
   return (
-    <div>
-        <h1>Hello World!</h1>
-    </div>
+    <Fragment>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </Fragment>
   )
 }
 
